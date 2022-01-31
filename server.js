e16fe6e7b8d909f9923d92d0b4b868afa03ce504
Kieran Harris <kharris19063@gmail.com>
2022-01-31 00:51:27 -0500

    Adding main server file, commenting out routes until they are added and functioning

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+// (require(".routes/api-routes.js"))(app);
+// (require(".routes/html-routes.js"))(app);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+})
